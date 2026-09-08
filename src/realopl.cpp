5a5df355adc0fc4172c2f3edde79cdcdb4e53ad0
Stian Skjelstad <stian.skjelstad@gmail.com>
2026-09-08 21:16:09 +0200

    oldvol not initialized.

--- src/realopl.cpp
+++ src/realopl.cpp
@@ -72,7 +72,7 @@ static __inline void outb(unsigned char value, unsigned short int port) {
 #endif
 
 CRealopl::CRealopl(unsigned short initport)
-  : adlport(initport), hardvol(0), bequiet(false), nowrite(false) {
+  : adlport(initport), hardvol(0), oldvol(0), bequiet(false), nowrite(false) {
   for (int i = 0; i < 22; i++) {
     hardvols[0][i][0] = 0;
     hardvols[0][i][1] = 0;
