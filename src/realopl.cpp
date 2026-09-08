7b97acdfe3c24dd8f7961f9f5afb24261caa6d16
Stian Skjelstad <stian.skjelstad@gmail.com>
2026-09-08 21:45:59 +0200

    gotperms not initialized

--- src/realopl.cpp
+++ src/realopl.cpp
@@ -73,6 +73,9 @@ static __inline void outb(unsigned char value, unsigned short int port) {
 
 CRealopl::CRealopl(unsigned short initport)
   : adlport(initport), hardvol(0), oldvol(0), bequiet(false), nowrite(false) {
+#ifdef linux
+  gotperms = false;
+#endif
   for (int i = 0; i < 22; i++) {
     hardvols[0][i][0] = 0;
     hardvols[0][i][1] = 0;
